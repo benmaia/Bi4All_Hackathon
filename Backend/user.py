eb61c557a3e496facd9b33f54cf28135c1c2b3c3
Buny <jbuny-fe@Bunys-MacBook-Pro.local>
2022-03-25 02:54:59 +0000

    02:54 da manhã

--- Backend/user.py
+++ Backend/user.py
@@ -1,6 +1,10 @@
 from roomback import *
 from gui import *
-
+import os 
+import threading
+def say():
+    os.system('say -v Joana "Bem vindo ao rumefóral"')
+threading.Thread(target=say).start()
 values = gui()
 values.room4all()
 
